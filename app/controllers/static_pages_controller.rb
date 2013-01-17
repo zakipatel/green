cd12b3b9b2c4e4f81d3e911666056b98f5d8e70f
Zaki Patel <zhpatel@gmail.com>
2013-01-16 23:14:54 -0500

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,10 @@ class StaticPagesController < ApplicationController
 
   def about
   end 
+
+  def contact 
+  end 
+
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Green::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  #get "static_pages/home"
   
+  #get "static_pages/about"
+  #get "static_pages/contact"
   
+  match '/signup',  to: 'users#new'
 
-
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  
+  #get "static_pages/help"
   resources :users
+
+  
 
 
   # The priority is based upon order of creation:
@@ -58,8 +65,8 @@ Green::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#index'
-
+  root :to => 'static_pages#home'
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
