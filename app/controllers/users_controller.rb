180c3fdaf98860c7cb74529df574e61917797d54
Zaki Patel <zhpatel@gmail.com>
2013-01-21 22:26:55 -0500

    ending ch8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,8 @@ class UsersController < ApplicationController
     respond_to do |format|
 
       if @user.save
+        sign_in @user
+        #flash[:success] = "Welcome to the Sample App!"
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Green::Application.routes.draw do
   #get "static_pages/contact"
   
   resources :users
-  
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
